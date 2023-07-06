12dceb74bca17bbd4a5c105d0d3f4bbebe1ddef4
Amajuoyi chidera <amajuoyichideradavid@gmail.com>
2023-07-06 17:47:55 +0100

    first version

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$!grul3_3q9l$_f43h0bh4kzb()-te)9du@57$dv84wmc5s)#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
